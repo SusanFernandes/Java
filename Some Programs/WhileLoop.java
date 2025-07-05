575b2c1dfd3fc7dafab7291849938695d4c0b8a8
SusanFernandes <susanfernandes1305@gmail.com>
2025-07-05 23:58:54 +0530

    changes

---
+++ Some Programs/WhileLoop.java
@@ -0,0 +1,12 @@
+class WhileLoop{
+
+    public static void main(String args[]){
+
+        int count = 1; //initialization
+        while (count<=100){  //condition
+            System.out.println(count);  //actual printed work
+            count +=1;  //updating the condition
+        }
+
+    }
+}
