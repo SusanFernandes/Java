90a3f1b6060845a70401653cbad4a90887d8b2b6
SusanFernandes <susanfernandes1305@gmail.com>
2025-07-21 12:36:41 +0530

    change

--- Some Programs/Fibonacci.java
+++ Some Programs/Fibonacci.java
@@ -18,6 +18,7 @@ class Fibonacci{
 
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number : ");
         int n = input.nextInt();
         solution(n);
     }
