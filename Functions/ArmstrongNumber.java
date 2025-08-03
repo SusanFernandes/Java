6cba1d7742078f48682effd3982acefe5d69bcc1
SusanFernandes <susanfernandes1305@gmail.com>
2025-08-03 14:45:38 +0530

    c

--- Functions/ArmstrongNumber.java
+++ Functions/ArmstrongNumber.java
@@ -34,3 +34,45 @@ class ArmstrongNumber{
         }
     }
 }
+
+
+// //alternate method:
+// class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Try programiz.pro");
+
+//         int a = 153;
+//         int armNum = 0;
+//         int dup = a;
+
+//         int count = 0;
+//         int b = a;
+
+//         // Count number of digits
+//         while (b > 0) {
+//             b /= 10;
+//             count++;
+//         }
+
+//         // Reset b for processing digits
+//         b = a;
+
+//         while (b > 0) {
+//             int i = b % 10;
+//             b /= 10;
+
+//             int j = 1;
+//             for (int k = 0; k < count; k++) {
+//                 j *= i;
+//             }
+
+//             armNum += j;
+//         }
+
+//         if (armNum == dup) {
+//             System.out.println(dup + " is an Armstrong Number.");
+//         } else {
+//             System.out.println(dup + " is not an Armstrong Number.");
+//         }
+//     }
+// }
