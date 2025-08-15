6dd7e6160fa7e468064809ddc2aadb98749b2754
SusanFernandes <susanfernandes1305@gmail.com>
2025-08-15 23:16:58 +0530

    c

---
+++ Arrays/SumAvg.java
@@ -0,0 +1,5 @@
+class SumAvg{
+    public static void main (String [] args){
+
+    }
+}
--- Arrays/first.java
+++ Arrays/first.java
@@ -1,7 +1,10 @@
 class first{
-
+    static int c = 9;
     public static void main(String [] args){
         int [] a = {1,2,7,6,5,3,4};
+        String str[]= new String[8];
+        int arr[][] = new int[4][5];
+        int arr2[][]={{1,2,4,5},{12,43,23,54, 4}};
         int [] b =new int[60]; //60 9s the size of the array.   
         for(int i =0 ; i <b.length; i++){
             System.out.print(i+ ". " +b[i] + " \n");
