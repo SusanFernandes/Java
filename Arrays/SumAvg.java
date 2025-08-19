bf6433dddc9c236f02f8f2111d7ba2c6e05c963f
SusanFernandes <susanfernandes1305@gmail.com>
2025-08-19 21:17:10 +0530

    new

--- Arrays/SumAvg.java
+++ Arrays/SumAvg.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class SumAvg{
     public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the size of the array");
+        int len = scanner.nextInt();
+        int nums[] = new int[len];
+        int i = 0;
+        while(i<len){
+            System.out.println("Please enter your element no. " + (i+1) + " : ");
+            nums[i]= input.nextInt();
+            i++;
+
+
+        }
+
+
 
     }
 }
