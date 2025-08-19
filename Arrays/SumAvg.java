836603cf4286f6a8ae0b153389aacddb39b272f9
SusanFernandes <susanfernandes1305@gmail.com>
2025-08-19 22:09:08 +0530

    c

--- Arrays/SumAvg.java
+++ Arrays/SumAvg.java
@@ -8,7 +8,7 @@ class SumAvg{
         int nums[] = new int[len];
         int i = 0;
         while(i<len){
-            System.out.println("Please enter your element no. " + (i+1) + " : ");
+            System.out.print("Please enter your element no. " + (i+1) + " : ");
             nums[i]= scanner.nextInt();
             i++;
         }
@@ -32,7 +32,7 @@ class SumAvg{
 
 
 //method for the average of all the elements in the array
-    public static long avg(int []a ){
+    public static double avg(int []a ){
         // long sum =0;
         // int count =0;
 
@@ -45,8 +45,8 @@ class SumAvg{
 
         // return Avg;
 
-        long sum = sum(a);
-        return (int)(sum/a.length);
+        double sum = sum(a);
+        return (sum/a.length);
 
     }
 }
