879cd98dd8ff6879ff2575b4cdd09c23c4720769
SusanFernandes <susanfernandes1305@gmail.com>
2025-09-25 02:11:25 +0530

    C

--- Arrays/DeleteElement.java
+++ Arrays/DeleteElement.java
@@ -8,6 +8,7 @@ class DeleteElement{
         System.out.print("Enter the size of the array: ");
         int a = sc.nextInt();
         int arr [] = new int [a];
+        //getting array input
         for(int i =0; i <arr.length; i++){
             System.out.print("Enter the element for index " + i+ " : ");
             arr[i] = sc.nextInt();
@@ -22,20 +23,50 @@ class DeleteElement{
 
         System.out.println("");
         System.out.print("Enter the number you want to delete : ");
-        int delete = sc.nextInt();
+        int numToDelete = sc.nextInt();
 
-        int deleteNum = newArr(arr, delete);
+        int [] newArr = deleteNumber(arr, numToDelete);
+
+        System.out.println("New Array is: ");
+        System.out.print("[ ");
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print(newArr[i] + " ");
+        }
+        System.out.println("]");
 
     }
 
-    public static int newArr(int arr[], int delete){
-        for (int i =0; i<arr.length; i++){
-            if(arr[i] == delete){
-                
+    public static int numOfOccurences(int []a, int num){
+        int count =0;
+        int i = 0;
+        while(i<a.length){
+            if (a[i]== num){
+                count ++;
             }
+            
+            i++;
+        }
+        return count;
+    
+    }
+
+    public static int [] deleteNumber(int arr[], int numToDelete){
+        int occ = numOfOccurences(arr, numToDelete);
+        if (occ ==0){
+            return arr;
+        }
+        int newSize = arr.length - occ;
+        int [] newArr = new int [newSize];
+        int i= 0, j=0;
+        while(i<arr.length){
+            if(arr[i] != numToDelete){
+                newArr[j] = arr[i];
+                j++;
+            }
+            i++;
         }
 
 
-        return 0;
+        return newArr;
     }
 }
